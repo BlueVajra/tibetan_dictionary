0df42e14eff75b82029916a84e575c3ab2570801
BlueVajra <cory.leistikow@gmail.com>
2014-08-03 18:42:05 -0600

    added binary file wkhtmltopdf-amd64 to enable heroku to generate pdfs

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf',
-  :wkhtmltopdf => "/Users/Cory/.rvm/gems/ruby-2.1.1/bin/wkhtmltopdf"
-}
+platform = RUBY_PLATFORM
+if platform.include?("64-linux")
+  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s}
+else
+  WickedPdf.config = { :wkhtmltopdf => "/Users/Cory/.rvm/gems/ruby-2.1.1/bin/wkhtmltopdf", :exe_path => '/usr/local/bin/wkhtmltopdf'}
+end
